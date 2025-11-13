ce981535d10fcae9f52999226830736987779444
R9jin <ticsrgt@gmail.com>
2025-11-13 15:30:00 +0800

    Commit of lab act 1

---
+++ Components/About.jsx
@@ -0,0 +1,22 @@
+import './style.css';
+function About() {
+    return (
+        <div className="card about-card">
+            <div className="profile-img-container">
+                <img
+                    src='/assets1/ProfilePic.jpg'
+                    alt="Profile"
+                    className="profile-img"
+                />
+            </div>
+            <h3>Roginne Geoffrey T. Ticao</h3>
+            <p>
+                A 3rd Year student currently taking <br />
+                BS in Computer Science in Pamantasan ng Cabuyao
+            </p>
+            <p>🎮 Interested in Game and Web Development 🎮</p>
+        </div>
+    );
+}
+
+export default About;
